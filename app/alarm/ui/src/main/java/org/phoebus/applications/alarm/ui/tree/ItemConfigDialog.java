cb7aac91c416e58b1fafb22732ae8e08303afde9
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-04-01 11:56:27 -0700

    Fix tag method

--- app/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/tree/ItemConfigDialog.java
+++ app/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/tree/ItemConfigDialog.java
@@ -238,7 +238,7 @@ class ItemConfigDialog extends Dialog<Boolean>
         config.setDisplays(displays.getItems());
         config.setCommands(commands.getItems());
         config.setActions(actions.getItems());
-        config.setTags(guidance.getTags());
+        config.setTags(tags.getTags());
 
         try
         {
